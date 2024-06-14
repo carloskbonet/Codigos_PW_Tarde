4bba073c8e3c2c6264f0d09535b85ad2e75b5187
Sabo <57690741+carloskbonet@users.noreply.github.com>
2024-06-14 17:29:00 -0300

    commit 3 / 14.06.2024

--- JavaScript/Exercicio 1/script/actions.js
+++ JavaScript/Exercicio 1/script/actions.js
@@ -5,10 +5,20 @@ const birthDate = document.getElementById('i-birthDate');
 const orders = document.getElementById('i-orders');
 const submit = document.getElementById('i-submit');
 
+// Divs
+const container = document.getElementById('container');
+
 submit.onclick = formSubmit;
 
 function formSubmit() {
+    const currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+
+    // Calcular a idade do usuário a partir do ano atual    
+
     console.log(userName.value);
     console.log(birthDate.value);
     console.log(orders.value);
+
+    container.style.display = 'none';
 }
