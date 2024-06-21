b3822c69bc2768f21d960b0eadc8d864dfbaa628
Sabo <57690741+carloskbonet@users.noreply.github.com>
2024-06-21 14:58:29 -0300

    commit 1 / 21.06.2024

--- JavaScript/Trivia/scripts/gameplay.js
+++ JavaScript/Trivia/scripts/gameplay.js
@@ -1,8 +1,68 @@
 
-perguntas = {
-    "1" : "Pergunta 1 ?"
-};
 
-respostas = {
-    "1" : "Resposta da pergunta 1."
-}
+// Alternativas de temas:
+// Programas de TV / Novelas / Filmes / etc...
+// Séries de sites pagos com streaming de video
+// Músicas / Estilos musicais
+// Esportes
+// Jogos / Animes
+
+answers = [
+    {
+        // MJ
+        question : 'Qual é o estilo musical ?',
+        a : 'Jazz',
+        b : 'Pop',
+        c : 'MPB',
+        d : 'Samba',
+        answer : 'b'
+    },
+    {   
+        // Ka
+        question : 'Qual é o estilo musical ?',
+        a : 'Jazz',
+        b : 'Pop',
+        c : 'MPB',
+        d : 'Samba',
+        answer : 'c'
+    },
+    {
+        // K F P
+        question : 'Quem é o cantor?',
+        a : 'Stevie Wonder',
+        b : 'James Hetfield',
+        c : 'Eddie Vedder',
+        d : "Jack Black",
+        answer : 'd'
+    },
+    {
+        // I U 2
+        question : 'Essa música vem de qual filme?',
+        a : 'Velozes e Furiosos 3 Desafio em Tóquio',
+        b : 'Irmão Urso 2',
+        c : 'A era do Gelo 3',
+        d : 'Rio 2',
+        answer : 'b'
+    },
+    {
+        // M C Ma
+        question : 'Quem é o cantor?',
+        a : 'Tim Maia',
+        b : 'MC Marcinho',
+        c : 'Padre Fabio de Melo',
+        d : 'Gal Costa',
+        answer : 'a'
+    },
+    {
+        //
+        question : 'Essa fala é o bordão de qual personagem?',
+        a : 'Jack Sparrow',
+        b : 'Jake o Cão',
+        c : 'Peter Griffin',
+        d : 'Homer Simpson',
+        answer : 'd'
+    }
+];
+
+
+
