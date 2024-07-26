48434d909445a78d52c0ed5334eadf75f39ef8f0
Sabo <57690741+carloskbonet@users.noreply.github.com>
2024-07-26 17:03:01 -0300

    commit 2 / 26.07.2024

--- Framework/exemplo-t/pages/user/register.tsx
+++ Framework/exemplo-t/pages/user/register.tsx
@@ -1,7 +1,50 @@
 import Head from "next/head";
 import styles from '@/styles/register.module.css';
+import { useState } from "react";
 
 export default function Register() {
+
+    const [ formData , setFormData ] = useState(
+    {
+        name: "",
+	    email: "",
+	    username: "",
+	    password: "",
+	    cPassword: ""
+    }
+    
+    );
+
+    function handleFormEdit(event:any , field:string) {
+        setFormData({
+            ...formData,
+            [field]: event.target.value
+        });
+
+        console.log(formData);
+    }
+
+    async function formSubmit(event:any) {
+        event.preventDefault();
+
+        try {
+            const response = await fetch(`/api/action/user/create` , {
+                method: 'POST',
+                headers: { 'Content-type':'application/json' },
+                body: JSON.stringify(formData)
+            });
+
+            const responseJson = await response.json();
+
+            alert(`${response.status} \n ${responseJson}`);
+
+        }
+        catch (err) {
+            console.log(err);
+        }
+
+    }
+
 
     return (
         <main id={styles.mainContainer} className='flex min-h-screen flex-col'>
@@ -15,13 +58,13 @@ export default function Register() {
             <div>
                 <h1 className={styles.text} >Página de Cadastro</h1>
 
-                <form className={styles.container}>
+                <form className={styles.container} onSubmit={formSubmit}>
 
-                    <input className={styles.input} type="text" placeholder="Nome (Opcional)" /><br />
-                    <input className={styles.input} type="email" placeholder="Email" /><br />
-                    <input className={styles.input} type="text" placeholder="Nome de Usuário" /><br />
-                    <input className={styles.input} type="password" placeholder="Senha" /><br />
-                    <input className={styles.input} type="password" placeholder="Confirmação de Senha" /><br />
+                    <input className={styles.input} type="text" placeholder="Nome (Opcional)" onChange={(event) => {handleFormEdit(event , 'name')}}  /><br />
+                    <input className={styles.input} type="email" placeholder="Email" onChange={(event) => {handleFormEdit(event , 'email')}} /><br />
+                    <input className={styles.input} type="text" placeholder="Nome de Usuário" onChange={(event) => {handleFormEdit(event , 'username')}} /><br />
+                    <input className={styles.input} type="password" placeholder="Senha" onChange={(event) => {handleFormEdit(event , 'password')}} /><br />
+                    <input className={styles.input} type="password" placeholder="Confirmação de Senha" onChange={(event) => {handleFormEdit(event , 'cPassword')}} /><br />
 
                     <input className={styles.sendBtn} type="submit" value="Criar Conta" />
 
